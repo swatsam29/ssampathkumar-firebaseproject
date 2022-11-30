d05fbb41a126570786ffa04dafb8d5ab828f3348
ssampathkumar <ssampathkumar@uco.edu>
2022-11-30 04:59:21 -0600

    save messages

--- public/controller/firestore_controller.js
+++ public/controller/firestore_controller.js
@@ -3,6 +3,8 @@ import { getFirestore, collection, addDoc, query, where, orderBy, getDocs } from
 const db = getFirestore();
 
 const TicTacToeGameCollection = 'tictactoe_game';
+const CommunityCollection = 'community_feed';
+
 
 export async function addTicTacToeGameHistory(gamePlay) {
     //gamePlay = {email, winner, moves, timestamp}
@@ -23,3 +25,24 @@ snapShot.forEach( doc => {
 });
 return history;
 }
+
+export async function addCommunity(textmessage) {
+    //textmessage = {email, message, timestamp}
+    //console.log("I was here");
+    await addDoc(collection(db, CommunityCollection), textmessage);
+}
+
+export async function getCommunity() {
+    let history= [];
+    const q = query(
+        collection(db, CommunityCollection),
+        orderBy('timestamp','desc'),
+    );
+const snapShot = await getDocs(q);
+snapShot.forEach( doc => {
+    const {email, message, timestamp } = doc.data();
+    history.push({email, message, timestamp});
+});
+return history;
+}
+
--- public/viewpage/community_page.js
+++ public/viewpage/community_page.js
@@ -4,10 +4,11 @@ import { unauthorizedAccess } from './unauthorized_access_message.js';
 import { currentUser } from '../controller/firebase_auth.js';
 import { info } from './util.js';
 import { DEV } from '../model/constants.js';
+import { addCommunity, getCommunity } from '../controller/firestore_controller.js';
 
 export function addEventListeners() {
     Elements.menus.community.addEventListener('click', () => {
-        history.pushState(null,null, routePath.COMMUNITY);
+        history.pushState(null, null, routePath.COMMUNITY);
         community_page();
     });
 }
@@ -21,23 +22,93 @@ export async function community_page() {
     const response = await fetch('/viewpage/templates/community_page.html', { cache: 'no-store' });
     html = await response.text();
     Elements.root.innerHTML = html;
-
     document.getElementById('button-create').addEventListener('click', create);
-
+    commenthistory();
 }
 
 async function create(event) {
-
-        let html = `
-        <form id= "form-add-reply" method= "post">
-            <textarea name= "content" required minlength= "3" placeholder= "Enter Message" ></textarea>
+    let html = `
+        <div>
+            <textarea id= "save-text" name= "content" required minlength= "3" placeholder= "Enter Message" ></textarea>
             <br>
-            <button type = "submit" class= "btn btn-outline-danger">Save</button>
-            <button type = "submit" class= "btn btn-outline-secondary">Cancel</button>
-        </form>
-        `;
-        document.getElementById('create').innerHTML = html;
+            <button id="save-button" type = "submit" class= "btn btn-outline-danger">Save</button>
+            <button id="cancel-button" type = "submit" class= "btn btn-outline-secondary">Cancel</button>
+        </div>
+            `;
+    document.getElementById('create').innerHTML = html;
+    document.getElementById('save-button').addEventListener('click', save);
+    document.getElementById('cancel-button').addEventListener('click', cancel);
 
+}
+function cancel(event) {
+    community_page();
+}
 
+async function save(event) {
+
+    let x = document.getElementById('save-text').value;
+    console.log(x);
+    //document.getElementById('save-button').innerHTML = html;
+    if(x!=""){
+        
     
+    const textmessage = {
+        email: currentUser.email,
+        message: x,
+        timestamp: Date.now(),
+    };
+    try {
+        await addCommunity(textmessage);
+        info('Message added', 'Text message added to firebase :D');
+        community_page();
+    } catch (e) {
+        info('Fail', `Failed to save message ${e}`);
+        if (DEV) console.log('failed to save:', e);
+    }
+}
+}
+
+async function commenthistory() {
+    let history;
+    let html = `
+    <tr>
+        <th>Message</th>
+        <th>Action</th>
+    </tr>
+    `;
+    try {
+        history = await getCommunity();
+        for (let i = 0; i < history.length; i++) {
+            html += `
+            <tr>
+            <td> 
+            <div>By: ${history[i].email} (Posted At ${new Date(history[i].timestamp).toLocaleString()})</div>  
+                <br>
+                ${history[i].message}
+            </td>
+            <td>
+            <button id="edit-button" type = "submit" class= "btn btn-outline-danger">Edit</button>
+            <button id="delete-button" type = "submit" class= "btn btn-outline-secondary">Delete</button>
+            </td>
+            <tr>
+            `;
+        }
+        document.getElementById('message-table').innerHTML = html;
+        document.getElementById
+        document.getElementById('edit-button').addEventListener('click', editbutton);
+        document.getElementById('delete-button').addEventListener('click', deletebutton);
+
+    } catch (e) {
+        info('Fail', `Failed to load message ${e}`);
+        if (DEV) console.log('failed to load', e);
+    }
+    function editbutton() {
+        community_page();
+    }
+    function deletebutton() {
+
+
+       document.getElementById('row').remove();
+
+    }
 }
